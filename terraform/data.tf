a57eb7be4dc8c01e209f527ac7010ebf70989a7d
Maksim <maksim@wsb-241-74b.local>
2022-01-03 17:30:09 +0300

    changes

--- terraform/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "remote_state" {
-  backend = "s3"
-  config = {
-    bucket = "remote-state.bucket"
-    key = "hosted_zone/terraform.tfstate"
-    region = "eu-central-1"
-   }
-}
